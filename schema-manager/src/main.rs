3c3f19d1847a6d5565230bb81de98ff33ba8e207
bowrocker <jon.anderson@joyent.com>
2019-10-16 17:28:01 -0400

    Add triton lookup for electric boray host.

--- schema-manager/src/main.rs
+++ schema-manager/src/main.rs
@@ -1,30 +1,32 @@
 // Copyright 2019 Joyent, Inc.
+use std::collections::HashMap;
+use std::fs;
 use std::io::{Error, ErrorKind};
 use std::net::{SocketAddr, TcpStream};
 use std::process;
 
 use clap::{Arg, App, value_t, crate_version, ArgMatches};
+use cmd_lib::{output, FunResult};
 use diesel::connection::SimpleConnection;
 use diesel::prelude::*;
 use rust_fast::client as fast_client;
 use rust_fast::protocol::{FastMessage, FastMessageId};
-use sapi::SAPI;
+use sapi::{SAPI, ZoneConfig};
 use serde::Serialize;
 use serde_json::{Value, json};
 use slog::{o, Drain, Logger};
 use std::sync::Mutex;
 use string_template::Template;
-use std::collections::HashMap;
 use utils::config;
 
+/* TODO
+    - populate service name per shard
+*/
+
 static APP: &'static str = "schema-manager";
-//static BORAY_CONFIG_FILE_PATH: &'static str = "/opt/smartdc/boray/etc/config.toml";
-static BORAY_CONFIG_FILE_PATH: &'static str = "./config.toml";
-static DEFAULT_SAPI_ADDRESS: &'static str = "10.77.77.137";
-const DEFAULT_SAPI_PORT: u32 = 2030;
-static DEFAULT_EB_ADDRESS: &'static str = "10.77.77.137";
+static BORAY_CONFIG_FILE_PATH: &'static str = "../etc/config.toml";
 const DEFAULT_EB_PORT: u32 = 2020;
-const SCHEMA_STR: &'static str = include_str!("schema.sql");
+const SCHEMA_STR: &'static str = "../etc/schema.sql";
 
 #[derive(Clone, Debug, Serialize)]
 pub struct MethodOptions {
@@ -35,9 +37,13 @@ fn init_sapi_client(sapi_address: String, log: Logger) -> Result<SAPI, Error> {
     Ok(SAPI::new(&sapi_address, 60, log.clone()))
 }
 
+fn read_schema_template() -> Result<String, Error> {
+    fs::read_to_string(SCHEMA_STR)
+}
+
 fn parse_vnodes(msg: &FastMessage) -> Result<(), Error> {
     let v: Vec<Value> = msg.data.d.as_array().unwrap().to_vec();
-    println!("v: {:#?}", v);
+    //println!("v: {:#?}", v);
     for vnode in v {
         for v in vnode.as_array().unwrap() {
             let vn =v.as_str().unwrap();
@@ -49,6 +55,21 @@ fn parse_vnodes(msg: &FastMessage) -> Result<(), Error> {
 }
 
 fn create_bucket_schemas(vnode: &str) -> Result<(), Error> {
+    let db_url = format_db_url();
+    let conn = establish_db_connection(db_url);
+    let schema_template = read_schema_template()?;
+    let template = Template::new(&schema_template);
+    let mut args = HashMap::new();
+    args.insert("vnode", vnode);
+    let exec_str = template.render(&args);
+
+    match conn.batch_execute(&exec_str) {
+        Ok(_) => Ok(()),
+        Err(e) => Err(std::io::Error::new(ErrorKind::Other, e))
+    }
+}
+
+fn format_db_url() -> String {
     let boray_config = config::read_file(BORAY_CONFIG_FILE_PATH);
     let db_url = format!("postgres://{}:{}@{}:{}/{}",
                          boray_config.database.user,
@@ -57,48 +78,19 @@ fn create_bucket_schemas(vnode: &str) -> Result<(), Error> {
                          boray_config.database.port,
                          boray_config.database.database
                          );
-    let conn = establish_connection(db_url);
-    let template = Template::new(SCHEMA_STR);
-    let mut args = HashMap::new();
-    args.insert("vnode", vnode);
-    let exec_str = template.render(&args);
-    println!("{}", exec_str);
-    match conn.batch_execute(&exec_str) {
-        Ok(_) => Ok(()),
-        Err(e) => Err(std::io::Error::new(ErrorKind::Other, e))
-    }
+    db_url
 }
 
-fn establish_connection(database_url: String) -> PgConnection {
+fn establish_db_connection(database_url: String) -> PgConnection {
 
     PgConnection::establish(&database_url)
         .expect(&format!("Error connecting to {}", database_url))
 }
 
-pub fn parse_opts<'a, 'b>(app: String) -> ArgMatches<'a> {
+fn parse_opts<'a, 'b>(app: String) -> ArgMatches<'a> {
    App::new(app)
         .version(crate_version!())
         .about("Tool to manage postgres schemas for boray")
-        .arg(Arg::with_name("eb_address")
-            .help("electric-boray address")
-            .long("eb_address")
-            .takes_value(true),
-        )
-        .arg(Arg::with_name("eb_port")
-            .help("electric-boray port")
-            .long("eb_port")
-            .takes_value(true),
-        )
-         .arg(Arg::with_name("sapi_address")
-            .help("sapi address")
-            .long("sapi_address")
-            .takes_value(true),
-        )
-         .arg(Arg::with_name("sapi_port")
-            .help("sapi port")
-            .long("sapi_port")
-            .takes_value(true),
-        )
         .arg(
             Arg::with_name("fast_args")
                 .help("JSON-encoded arguments for RPC method call")
@@ -109,10 +101,9 @@ pub fn parse_opts<'a, 'b>(app: String) -> ArgMatches<'a> {
         .get_matches()
 }
 
-pub fn response_handler(msg: &FastMessage) -> Result<(), Error> {
+fn vnode_response_handler(msg: &FastMessage) -> Result<(), Error> {
     match msg.data.m.name.as_str() {
         "getvnodes" => {
-            println!("msg: {:#?}", msg);
             parse_vnodes(msg)?;
         }
         _ => println!("Received unrecognized {} response", msg.data.m.name),
@@ -121,13 +112,16 @@ pub fn response_handler(msg: &FastMessage) -> Result<(), Error> {
     Ok(())
 }
 
-pub fn run(
-    eb_address: String,
-    eb_port: u32,
+fn run(
     args: Value,
-    _sapi: SAPI) -> Result<(), Error>
+    log: Logger) -> Result<(), Box<dyn std::error::Error>>
 {
-    let eb_endpoint = [eb_address, String::from(":"), eb_port.to_string()]
+    let sapi_url = get_sapi_url()?;
+    let sapi = init_sapi_client(sapi_url, log.clone())?;
+    let zone_config = get_zone_config(sapi)?;
+    let eb_address = zone_config.metadata.electric_boray_hostname;
+
+    let eb_endpoint = [eb_address, String::from(":"), DEFAULT_EB_PORT.to_string()]
         .concat()
         .parse::<SocketAddr>()
         .unwrap_or_else(|e| {
@@ -143,19 +137,31 @@ pub fn run(
     let mut msg_id = FastMessageId::new();
 
     for s in shards {
-        println!("shard: {:#?}", json!([s]));
         let vnode_method = String::from("getvnodes");
         fast_client::send(vnode_method,
                           json!([s]),
                           &mut msg_id,
                           &mut stream).and_then(
-            |_| fast_client::receive(&mut stream, response_handler),
+            |_| fast_client::receive(&mut stream, vnode_response_handler),
         )?;
     }
     Ok(())
 }
 
-pub fn main() -> Result<(), Error> {
+fn get_zone_uuid() -> FunResult {
+    output!("/usr/sbin/zoneadm list")
+}
+
+fn get_sapi_url() -> FunResult {
+    output!("/usr/sbin/mdata-get SAPI_URL")
+}
+
+fn get_zone_config(sapi: SAPI) -> Result<ZoneConfig, Box<dyn std::error::Error>> {
+    let zone_uuid = get_zone_uuid()?;
+    sapi.get_zone_config(&zone_uuid)
+}
+
+pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     let plain = slog_term::PlainSyncDecorator::new(std::io::stdout());
     let log = Logger::root(
         Mutex::new(slog_term::FullFormat::new(plain).build()).fuse(),
@@ -163,15 +169,8 @@ pub fn main() -> Result<(), Error> {
     );
 
     let options = parse_opts(APP.to_string());
-    let sapi_address = options.value_of("sapi_address").unwrap_or(DEFAULT_SAPI_ADDRESS).to_string();
-    let _sapi_port = value_t!(options, "sapi_address", u32).unwrap_or(DEFAULT_SAPI_PORT);
-    let sapi = init_sapi_client(sapi_address, log.clone())?;
     let args = value_t!(options, "fast_args", Value).unwrap_or_else(|e| e.exit());
-    println!("args: {:#?}", args);
-    let eb_address = options.value_of("eb_address").unwrap_or(DEFAULT_EB_ADDRESS).to_string();
-    let eb_port = value_t!(options, "eb_port", u32).unwrap_or(DEFAULT_EB_PORT);
 
-    run(eb_address, eb_port, args, sapi)?;
+    run(args, log)?;
     Ok(())
-    // populate service name per shard
 }
