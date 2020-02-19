558e84e3586311628a9854470be89392762d6df7
Kelly McLaughlin <kelly.mclaughlin@joyent.com>
2020-02-19 10:30:13 -0700

    Distinguish inner delimiters from the outer migration delimiter

--- migrations/public/0001-0001-helper-functions.sql
+++ migrations/public/0001-0001-helper-functions.sql
@@ -6,7 +6,7 @@ CREATE OR REPLACE FUNCTION list_all_objects(lmt int DEFAULT 100)
 RETURNS TABLE(schma text, id uuid, name text, owner uuid, bucket_id uuid, created
 timestamptz, modified timestamptz, content_length
 bigint, content_md5 bytea, content_type text, headers hstore, sharks text[],
-properties jsonb) AS $$
+properties jsonb) AS $INNER$
 DECLARE
   schema RECORD;
 BEGIN
@@ -17,10 +17,10 @@ BEGIN
       format('SELECT ''%I'', id, name, owner, bucket_id, created, modified, content_length, content_md5::bytea, content_type, headers, sharks, properties FROM %I.manta_bucket_object LIMIT %L', schema.schema_name, schema.schema_name, lmt);
   END LOOP;
 END;
-$$ LANGUAGE plpgsql;
+$INNER$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION list_all_buckets(lmt int DEFAULT 100)
-RETURNS TABLE(schma text, id uuid, name text, owner uuid, created timestamptz) AS $$
+RETURNS TABLE(schma text, id uuid, name text, owner uuid, created timestamptz) AS $INNER$
 DECLARE
   schema RECORD;
 BEGIN
@@ -31,13 +31,13 @@ BEGIN
       format('SELECT ''%I'', id, name, owner, created FROM %I.manta_bucket LIMIT %L', schema.schema_name, schema.schema_name, lmt);
   END LOOP;
 END;
-$$ LANGUAGE plpgsql;
+$INNER$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION bucket_listing(o_id uuid, b_id uuid, lmt int DEFAULT 100)
 RETURNS TABLE(schma text, id uuid, name text, owner uuid, bucket_id uuid, created
 timestamptz, modified timestamptz, content_length
 bigint, content_md5 bytea, content_type text, headers hstore, sharks text[],
-properties jsonb) AS $$
+properties jsonb) AS $INNER$
 DECLARE
   schema RECORD;
 BEGIN
@@ -49,10 +49,10 @@ BEGIN
 bucket_id = %L LIMIT %L', schema.schema_name, schema.schema_name, o_id, b_id, lmt);
   END LOOP;
 END;
-$$ LANGUAGE plpgsql;
+$INNER$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION list_all_deleted_objects(lmt int DEFAULT 1000)
-RETURNS TABLE(schma text, id uuid, owner uuid, bucket_id uuid, name text, created timestamptz, modified timestamptz, content_length bigint, content_md5 bytea, content_type text, headers hstore, sharks text[], properties jsonb) AS $$
+RETURNS TABLE(schma text, id uuid, owner uuid, bucket_id uuid, name text, created timestamptz, modified timestamptz, content_length bigint, content_md5 bytea, content_type text, headers hstore, sharks text[], properties jsonb) AS $INNER$
 DECLARE
         schema RECORD;
 BEGIN
@@ -63,10 +63,10 @@ BEGIN
                   format('SELECT ''%I'', id, owner, bucket_id, name, created, modified, content_length, content_md5, content_type, headers, sharks, properties FROM %I.manta_bucket_deleted_object LIMIT %L', schema.schema_name, schema.schema_name, lmt);
       END LOOP;
 END;
-$$ LANGUAGE plpgsql;
+$INNER$ LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION list_all_deleted_buckets(lmt int DEFAULT 100)
-RETURNS TABLE(schma text, id uuid, name text, owner uuid, created timestamptz) AS $$
+RETURNS TABLE(schma text, id uuid, name text, owner uuid, created timestamptz) AS $INNER$
 DECLARE
   schema RECORD;
 BEGIN
@@ -77,7 +77,7 @@ BEGIN
       format('SELECT ''%I'', id, name, owner, created FROM %I.manta_bucket_deleted_bucket LIMIT %L', schema.schema_name, schema.schema_name, lmt);
   END LOOP;
 END;
-$$ LANGUAGE plpgsql;
+$INNER$ LANGUAGE plpgsql;
 
 INSERT INTO migrations (major, minor, note) VALUES (1, 1, 'Create helper functions for operators');
 
